73207351c9730d0b750269314f56fe8b99e845b8
nafiz1001 <nafiz.islam1001@gmail.com>
2021-07-29 18:02:45 -0400

    more than one root_dir

--- PerformanceEstimator.py
+++ PerformanceEstimator.py
@@ -26,23 +26,24 @@ pattern_and_complexity_list : list[tuple[typing.Pattern, int]] = [
 ]
 
 # starting directory for finding *.yaml files
-root_dir = sys.argv[1] if len(sys.argv) > 1 else "."
+root_dirs = sys.argv[1:] if len(sys.argv) > 1 else "."
 
 # list of path to yaml and its associated total complexity
 yaml_path_and_complexity_list = list[tuple[pathlib.Path, int]]()
 
 try:
-    for yaml_path in yaml_paths(root_dir):
-        yaml_complexity = 0
-        for yaml_line in open(yaml_path):
-            for pattern, complexity in pattern_and_complexity_list:
-                if (m := pattern.search(yaml_line)):
-                    # m.groups(): all matches to the pattern in yaml_line
-                    yaml_complexity += len(m.groups()) * complexity
+    for root_dir in root_dirs:
+        for yaml_path in yaml_paths(root_dir):
+            yaml_complexity = 0
+            for yaml_line in open(yaml_path):
+                for pattern, complexity in pattern_and_complexity_list:
+                    if (m := pattern.search(yaml_line)):
+                        # m.groups(): all matches to the pattern in yaml_line
+                        yaml_complexity += len(m.groups()) * complexity
 
-        yaml_path_and_complexity_list.append((yaml_path, yaml_complexity))
+            yaml_path_and_complexity_list.append((yaml_path, yaml_complexity))
 
-    for yaml_path, yaml_complexity in sorted(yaml_path_and_complexity_list, key=lambda y_c: y_c[1], reverse=True):
-        print(f"{yaml_path}: {yaml_complexity}")
+        for yaml_path, yaml_complexity in sorted(yaml_path_and_complexity_list, key=lambda y_c: y_c[1], reverse=True):
+            print(f"{yaml_path}: {yaml_complexity}")
 except BrokenPipeError:
     pass
